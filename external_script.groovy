744e8983ecbf3b87bb9bef45784dda3bed8a2e7f
Dmitriy Ten <ten_dmitriy@mail.ru>
2021-10-21 22:30:12 +0300

    added mvn deployment to nexus

--- external_script.groovy
+++ external_script.groovy
@@ -1,6 +1,6 @@
 def buildApp() {
     echo 'building app'
-    sh 'mvn --version'
+    sh 'mvn clean package'
     echo 'building ${NEW_VERSION} - var is not installed'
     echo "building ${NEW_VERSION} - !var is installed!"
     echo "Creds: ${CREDS}, user: ${CREDS_USR}, passwd: ${CREDS_PSW}"}
@@ -14,7 +14,7 @@ def deployApp() {
     withCredentials([
             usernamePassword(credentialsId: 'github-creds', usernameVariable: 'USER', passwordVariable: 'PASSWORD')
     ]){
-        sh "echo user"
+        sh "mvn deploy -s settings.xml"
         sh 'echo user: $USER, pswd: $PASSWORD'
         sh "echo user: $USER, pswd: $PASSWORD"
         sh "echo user: $var_name"
